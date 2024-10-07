40c4eb3994fbadc89ab995a93d380d86262031cc
harshit.sharma@incture.com <harshit.sharma@incture.com>
2024-10-07 15:13:57 +0530

    feat: added dashboard routes

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Lusitana } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+
+export const lusitanta = Lusitana({ weight: ["400", "700"], subsets: ["latin"] });
